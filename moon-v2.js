22ae39267e50cbe0650cfa5cc734fbefb97893a4
TheGoobermench <jwbarrett16@gmail.com>
2020-11-11 22:01:00 -0600

    Found the new cycle

--- moon-v2.js
+++ moon-v2.js
@@ -1,6 +1,6 @@
 let t = 0
 let s = 0 
-let x = 99625
+let x = 0
 
 let annu = Math.cos(.3222 * (t - 3))// 19.5 shift 3
 let oki = Math.cos(.1381 * (s - 6)) // 45.5 shift 6
@@ -9,7 +9,7 @@ let taro = Math.cos(.0869 * (x - 25)) // 72.3 shift 25
 const solveMoons = () => {
     // Solve Taro
     taro = Math.cos(.0869 * (x - 25))
-    while(taro >= -0.99999999) {
+    while(taro >= -0.9999999) {
         console.log(`Taro = ${taro} - ${x}`);
         x += .00347;
         taro = Math.cos(.0869 * (x - 25))
@@ -18,7 +18,7 @@ const solveMoons = () => {
     // Solve Oki
     s = x - 1;
     oki = Math.cos(.1381 * (s - 6))
-    while(oki >= -0.9999999) {
+    while(oki >= -0.999999) {
         console.log(`Oki = ${oki} - ${s}`);
         s += .00347;
         oki = Math.cos(.1381 * (s - 6))
@@ -32,7 +32,7 @@ const solveMoons = () => {
     // Solve Annu
     t = x - 1
     annu = Math.cos(.3222 * (t - 3))
-    while(annu >= -0.9999999) {
+    while(annu >= -0.999999) {
         console.log(`Annu = ${annu} - ${t}`);
         t += .00347;
         annu = Math.cos(.3222 * (t - 3))
@@ -40,7 +40,6 @@ const solveMoons = () => {
 
         if(t - x > 1 || Math.abs(t - s) > 1) {
             x += 72;
-            console.log('HERE')
             solveMoons()
         }
     }
